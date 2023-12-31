9c1e028a9fe6973709795f6c27bbd3729e2091c9
Macbook pro <macbookpro@MacBook-Pro-de-Macbook.local>
2023-12-31 16:35:02 +0000

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "apijapnasi",
+  HOST: "91.216.107.164",
+  USER: "dkrin2191788",
+  PASSWORD: "eC6*SE3qjN9KbNW",
+  DB: "dkrin2191788_1iguqg",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -11,3 +11,8 @@ module.exports = {
     idle: 10000
   }
 };
+
+// Serveur Mysql : 91.216.107.164 (à la place de localhost quand vous installez un CMS)
+// Nom de la base Mysql : dkrin2191788_1iguqg
+// Identifiant / login : dkrin2191788
+// Mot de passe : eC6*SE3qjN9KbNW
